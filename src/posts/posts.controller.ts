768ffa2cae203363e39ff8c0204fe7019b8b7b7e
나황제 <na03266@livewalk.co.kr>
2024-02-13 11:36:43 +0900

    update 5개의 요청

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +19,129 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 혜린',
+    content: '노래 연습하고 있는 혜린',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합 운동장에서 공연중인 로제',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+];
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET /posts
+  //    모든 posts를 다 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 1000000,
-      commentCount: 999999,
-    };
+  getPosts() {
+    return posts;
+  }
+
+  // 2) GET /posts/:id
+  //    id에 해당되는 post를 가져온다.
+  //    예를 들어서 id=1일 경우 id가 1인 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    } else {
+      return post;
+    }
+  }
+  // 3) POST /posts
+  //    POST 를 생성한다.
+  //  이름짓기 : 메서드 이름 + 패스이름
+  @Post()
+  postPost(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post : PostModel = {
+      id: posts[posts.length -1].id +1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0
+    }
+
+    posts = [
+      ...posts,
+      post,
+    ]
+
+    return post;
+  }
+  // 4) PUT /posts/:id
+  //    id에 해당되는 POST를 변경한다.
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+    ){
+    const post = posts.find(post => post.id === +id)
+
+    if(!post){
+      throw new NotFoundException
+    }
+
+    if(author){
+      post.author = author;
+    }
+
+    if(title){
+      post.title = title
+    }
+
+    if(content){
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => prevPost.id === +id ? post : prevPost)
+
+    return post;
+  }
+
+  // 5) DELETE /posts/:id
+  //    id에 해당되는 POST를 삭제한다.
+
+  @Delete(':id')
+  deletePost(
+    @Param('id') id: string
+  ){
+    
+    const post = posts.find(post => post.id === +id)
+
+    if(!post){
+      throw new NotFoundException
+    }
+
+    posts = posts.filter(post => post.id !== +id)
+
+    return id;
   }
 }
