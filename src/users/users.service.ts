538c4f2db134be383860d7b41ddf4a30fdd6a9a2
나황제 <na03266@livewalk.co.kr>
2024-02-19 19:38:22 +0900

    Update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,29 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersModel } from './entities/user.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(UsersModel)
-        private readonly userRepository: Repository<UsersModel>,
-    ){}
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly userRepository: Repository<UsersModel>,
+  ) {}
 
-    async createUser(nickname:string, email:string, password: string){
-        const user = this.userRepository.create({
-            nickname,
-            email,
-            password,
-        })
+  async createUser(user: Pick<UsersModel, 'email' | 'nickname' | 'password'>) {
+    // 1) nickname 중복이 없는지 확인
+    // exist() -> 만약에 조건에 해당되는값이 있으면 true 반환
+    const nicknameExists = await this.userRepository.exists({
+      where: {
+        nickname: user.nickname,
+      },
+    });
 
-        const newUser = await this.userRepository.save(user);
-
-        return newUser
-
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname 입니다.');
     }
 
-    async getAllUsers(){
-        return this.userRepository.find()
+    const emailExists = await this.userRepository.exists({
+      where: {
+        email: user.email,
+      },
+    });
+
+    if (emailExists) {
+      throw new BadRequestException('이미 존재하는 Email 입니다.');
     }
+
+    const userObj = this.userRepository.create({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    });
+
+    const newUser = await this.userRepository.save(user);
+
+    return newUser;
+  }
+
+  async getAllUsers() {
+    return this.userRepository.find();
+  }
+
+  async getUserByEmail(email: string) {
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+  }
 }
